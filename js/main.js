74d7c4a7defa8264a432f6be72f99c89ae36bf06
r444sh <feararteezy@gmail.com>
2023-02-21 08:41:57 +0700

    Finished adaptive

--- js/main.js
+++ js/main.js
@@ -8,8 +8,20 @@ owl.owlCarousel({
     center: true,
     loop: true,
     margin: 30,
-    startPosition: 1,
-    items: 3,
+    startPosition: 0,
+    items: 1,
+    responsive : {
+
+        540: {
+            items: 3,
+            startPosition: 1,
+        },
+        
+        1200: {
+            items: 3,
+            margin: 30,
+        }
+    }
 });
 
 $('.slider__btn--prev').click(function() {
